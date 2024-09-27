d3142443c9988070eb932092b6527423103a96fa
Jacob Long <jacob.long@sc.edu>
2024-09-26 23:58:24 -0400

    Tweak

--- crawl.R
+++ crawl.R
@@ -24,45 +24,8 @@ if (file.exists(urls_file) && file.info(urls_file)$size > 0) {
 out <- retrieve_crossref_issn_data(
     issn_list=journals$issn, 
     start_date=crawl_start_date, 
-    end_date=crawl_end_date, 
-    verbose=TRUE)
-
-# Remove duplicates
-out <- out[!duplicated(out$url),] 
-# Remove past paers
-out <- out[!(out$url %in% past_urls$url), ]
-if(is.null(out)) quit(save="no")
-
-# Cleanup data
-out$abstract <- strip_html(out$abstract)
-out$abstract <- gsub("^(Abstract|ABSTRACT) ", "", out$abstract)
-out$title <- strip_html(out$title)
-out$doi <- extract_doi_id(out$url)
-
-# Merge in journal information 
-out <- merge(out, journals, by="issn")
-
-# Apply standard filter flags 
-out <- add_standard_filter(out) 
-
-# Filter flags: Multidisciplinary journals 
-if(field=="multidisciplinary"){
-    out_lst <- split(out, out$filter_function) 
-    out_lst <- lapply(out_lst, dispatch_special_filter ) 
-    out <- do.call(rbind, out_lst)
-    out$filter <- apply(out, 1, function(x){
-        tryCatch(
-            {add_multidisciplinary_filter(x)
-            }, error = function(msg){
-                return(-1)
-            })
-        })
-    rownames(out) <- NULL
-    } 
-
-# Output JSON
-out_json <- render_json(out, date=as.Date(now)) 
-write(out_json, paste0("./output/", field, ".json"))
+    end_date=crawl_end_date
+)
 
 # Update past urls
 write.table(out[,"url"], 
@@ -72,7 +35,8 @@ write.table(out[,"url"],
     append=TRUE, 
     quote=FALSE, 
     col.names=FALSE,
-    row.names=FALSE)
+    row.names=FALSE
+)
 
 # Write journal short list
 journals_out <- unique(journals[,c("journal_full","journal_short")])
@@ -95,3 +59,7 @@ if (file.exists(json_file) && file.info(json_file)$size > 0) {
 }
 
 cat("JSON data written successfully.\n")
+
+# Additional debugging: Check the contents of the written file
+written_json <- readLines(json_file)
+cat("Contents of the written JSON file:\n", paste(written_json, collapse="\n"), "\n")
